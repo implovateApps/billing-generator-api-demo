6b94d62e68ef30c4f42c6541ea57c4a591ec9cdd
implovateApps <63191508+implovateApps@users.noreply.github.com>
2026-02-24 11:46:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from google.cloud import tasks_v2
 app = Flask(__name__)
 
 # --- CONFIGURATION ---
-PROJECT_ID = "methodical-ace-482313-m2"       # <-- Change this!
+PROJECT_ID = "YOUR_PROJECT_ID"       # <-- Change this!
 LOCATION = "us-central1"             # <-- Change this!
-QUEUE_ID = "billing-queue"
-WEBHOOK_URL = "https://webhook.site/00194eb7-606a-47f1-9bdf-ad41c3050c84" # <-- Paste your webhook.site URL here!
+QUEUE_ID = "YOUR_QUEUE_ID"
+WEBHOOK_URL = "YOUR_WEBHOOK_URL" # <-- Paste your webhook.site URL here!
 
 client = tasks_v2.CloudTasksClient()
 parent = client.queue_path(PROJECT_ID, LOCATION, QUEUE_ID)
@@ -19,7 +19,7 @@ def generate_bills():
     print("🚨 SCHEDULER WOKE US UP! Generating 20 user bills...")
     
     # Simulate generating 20 user payments
-    for i in range(1, 21):
+    for i in range(1, 61):
         payload = {"user_id": f"User_{i}", "amount_due": 14.99, "plan": "Netflix Premium"}
         
         # Construct the task request
